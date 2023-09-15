8aa6286661b4055ad47b48f0641c54b4dedab6fe
arun06001 <arun06001@E-PW0234KE>
2023-09-15 10:20:35 +0200

    Add project files.

---
+++ Uppgift 2.3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till [BILHUTHYRNINGSFIRMA].");
+            Console.WriteLine("Hur många dagar vill du hyra bilen?");
+            byte dagar = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många kilometer har du tänkt köra?");
+            int kilometer = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Det kommer att kosta {300 + (dagar - 1)*500 + kilometer} kronor.");
+
+            Console.ReadKey();
+        }
+    }
+}
